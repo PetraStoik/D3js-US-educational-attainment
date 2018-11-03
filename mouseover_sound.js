333752a484a290ab18c1c891576839f2b60249c0
Stoikopoulou Petroula <44209860+PetraStoik@users.noreply.github.com>
2018-11-03 19:10:38 +0200

    Update mouseover_sound.js

--- mouseover_sound.js
+++ mouseover_sound.js
@@ -1,1 +1,10 @@
-<a href="about.htm" onmouseover="playclip();">Mouseover Sound Link</a><br>
+
+function playclip() {
+  if (document.all) {
+      document.all.sound.src = "18227__andbur__mic-off.mp3";
+  }
+  else {
+    var audio = document.getElementsByTagName("audio")[0];
+    audio.play();
+  }
+}
